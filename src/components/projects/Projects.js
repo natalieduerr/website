6920758f2cbf1ecc0e336b07e1d1b2952592e6da
natalieduerr <natalieduerr98@gmail.com>
2020-05-19 16:17:54 -0400

    film page and minor fixes

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -25,7 +25,7 @@ export default class Projects extends React.Component {
                 <ProjectCard name={'Mass Hike'} type={'Research, UX Design, and Visual Design'} link={"mass-hike"} img={"masshike.svg"}></ProjectCard>
                 <ProjectCard name={'Contuity'} type={'Research, UX Design, and Development'} link={"contuity"} img={"contuity.png"}></ProjectCard>
                 <ProjectCard name={'Ordering on BevSpot'} type={'Research, UX Design, and Visual Design'} link={"bevspot"} img={"bevspot.svg"}></ProjectCard>
-                <ProjectCard name={'Agnès Varda Poster'} type={'Typography, Print Design'} link={"agnes"} img={"agnes.png"}></ProjectCard>
+                <ProjectCard name={'A Year in Film'} type={'Typography, Info Design'} link={"film"} img={"film.svg"}></ProjectCard>
                 <ProjectCard name={'Drink Price Tool Landing Page'} type={'Web Design and Development'} link={"dpt"} img={"dpt-pina.svg"}></ProjectCard>
                 <ProjectCard name={'Online Resources'} type={'UX Design and Visual Design'} link={"online-resources"} img={"resources.svg"}></ProjectCard>
               </Grid>
--- src/routes.js
+++ src/routes.js
@@ -12,7 +12,7 @@ import Projects from "./components/projects/Projects";
 import MassHike from "./components/projects/detail/MassHike";
 import Contuity from "./components/projects/detail/Contuity";
 import BevSpot from "./components/projects/detail/BevSpot";
-import Agnes from "./components/projects/detail/Agnes";
+import Film from "./components/projects/detail/Film";
 import DPT from "./components/projects/detail/DPT";
 import OnlineResources from "./components/projects/detail/OnlineResources";
 
@@ -34,7 +34,7 @@ const Routes = props => {
                                 <Route exact path="/projects/mass-hike" render={() => <MassHike />} />
                                 <Route exact path="/projects/contuity" render={() => <Contuity />} />
                                 <Route exact path="/projects/bevspot" render={() => <BevSpot />} />
-                                <Route exact path="/projects/agnes" render={() => <Agnes />} />
+                                <Route exact path="/projects/film" render={() => <Film />} />
                                 <Route exact path="/projects/dpt" render={() => <DPT />} />
                                 <Route exact path="/projects/online-resources" render={() => <OnlineResources />} />
                             </Switch>
